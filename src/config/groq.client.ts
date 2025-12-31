c9911fc8c273c3cd98ce0e320275dc274606dd25
Rinki Das <rinroy351@gmail.com>
2025-12-31 22:21:04 +0530

    test sd

--- src/config/groq.client.ts
+++ src/config/groq.client.ts
@@ -1,5 +1,5 @@
 import Groq from "groq-sdk";
 
 export const groq = new Groq({
-  apiKey: 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,
+    apiKey: process.env.GROQ_API_KEY,
 });
--- src/config/supabase.client.ts
+++ src/config/supabase.client.ts
@@ -1,7 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
-
  
-const supabaseUrl = "https://tdmlvpjcyocwpvxzyuof.supabase.co";
-const supabaseAnonKey = "sb_publishable_sSAnpDzOZMZGf9H8yH-NVg_l5vTuEXp";
+const supabaseUrl = process.env.SUPABASE_URL;
+const supabaseAnonKey = process.env.SUPABASE_ANON_KEY;
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
