187058b18b47c208ba07ee52fc618292660cc6cf
rinki-das <rinroy351@gmail.com>
2025-12-31 22:58:36 +0530

    fix: allow cors

--- src/app.ts
+++ src/app.ts
@@ -6,7 +6,11 @@ export const app = express();
 
 app.use(
   cors({
-    origin: "*",
+    origin: [
+      "http://localhost:5173",
+      "https://support-backend-jyvj.onrender.com",
+    ],
+    credentials: true,
   })
 );
 
